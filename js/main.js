ed6358b2e87e5fb5c0e8e3332bdd5830f4141e49
brunoharlein <bruno@harlein.com>
2020-04-08 14:51:37 +0200

    create cards game

--- js/main.js
+++ js/main.js
@@ -29,3 +29,28 @@ function randomizeColors() {
     return 0.5 - Math.random()
   });
 }
+
+//Create a card element to be displayed on the screen
+// Créer une carte à afficher à l'écran
+function createCard() {
+  var card = document.createElement('div');
+  card.className = "col-sm-2";
+  card.innerHTML = "<div class='card my-2' style='background-color: black'></div>";
+  return card;
+}
+
+//Create 12 card elements on the screen
+// Créez 12 cartes à l'écran
+function addCards() {
+  var boardGame = document.getElementById("boardGame");
+  for (var i = 0; i < 12; i++) {
+    var card = createCard();
+    boardGame.appendChild(card);
+  }
+}
+
+//Change a card color
+// Changer la couleur d'une carte
+function changeCardColor(card, color) {
+  card.style.background = color;
+}
