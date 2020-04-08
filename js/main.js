67f9e1e89ebae31061f54a998ad82fc131994a89
brunoharlein <bruno@harlein.com>
2020-04-08 15:41:07 +0200

    check-delette-turn card

--- js/main.js
+++ js/main.js
@@ -54,3 +54,68 @@ function addCards() {
 function changeCardColor(card, color) {
   card.style.background = color;
 }
+
+//Game Functions
+
+//Function to start the time
+// Fonction pour démarrer le temps
+function timer() {
+  var seconds = 30;
+  var timer = document.getElementById("timer");
+  timer.innerHTML = "<i class='fas fa-hourglass-start ml-2'></i> Temps restant : " + seconds + " s";
+  setInterval(function(){
+    if(seconds >= 1) {
+      seconds --;
+      timer.innerHTML = "<i class='fas fa-hourglass-start ml-2'></i> Temps restant : " + seconds + " s";
+    }
+    else {
+      restartGame(false);
+    }
+  }, 1000);
+}
+
+//Store the selected color and the card element in arrays
+// Stocke la couleur sélectionnée et l'élément de carte dans des tableaux
+function registerAnswers(userAnswer, clickedCard) {
+    userAnswers.push(userAnswer);
+    clickedCards.push(clickedCard);
+}
+
+//Check if the the user answer is correct and clear the answers and the cards if necessary
+// Vérifier si la réponse de l'utilisateur est correcte et effacer les réponses et les fiches si nécessaire
+function handleAnswers() {
+  if(userAnswers[0] !== userAnswers[1]) {
+    clearCards();
+    clearAnswers();
+  }
+  else {
+    addPoint();
+    clearAnswers(true);
+  }
+}
+
+//Turn the selected cards into black cards
+// Transforme les cartes sélectionnées en cartes noires
+function clearCards() {
+  setTimeout(function(){
+    changeCardColor(clickedCards[0], "black");
+    changeCardColor(clickedCards[1], "black");
+  }, 1500);
+}
+
+//Clears the arrays containing the user answers and the clicked cards
+// Efface les tableaux contenant les réponses des utilisateurs et les cartes cliquées
+function clearAnswers(madePoint = false) {
+  if(!madePoint){
+    setTimeout(function(){
+      userAnswers = [];
+      clickedCards = [];
+      allowClick = true;
+    }, 1600);
+  }
+  else  {
+    userAnswers = [];
+    clickedCards = [];
+    allowClick = true;
+  }
+}
